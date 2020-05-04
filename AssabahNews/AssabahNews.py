a9f3be9ed36c461b9fe3311e4444fff4635c710d
FahdZaghdoudi <zaghdoudi.f28@gmail.com>
2020-05-04 23:55:03 +0100

    Update AssabahNews.py
    Improving execution time

--- AssabahNews/AssabahNews.py
+++ AssabahNews/AssabahNews.py
@@ -12,7 +12,6 @@ response = requests.get(base_site)
 # Extracting the HTML
 html = response.content
 
-# Convert HTML to a BeautifulSoup object. This will allow us to parse out content from the HTML more easily.
 # Using the default parser as it is included in Python
 soup = BeautifulSoup(html, "html.parser")
 
@@ -38,7 +37,6 @@ relative_urls = [link.get('href') for link in links]
 # Transforming to absolute path URLs
 full_urls = [urljoin(base_site, url) for url in relative_urls]
 
-
 #to avoid limiting the number of requests, we should tellpython to wait between each request
 #we importtime library
 import time
@@ -47,38 +45,29 @@ article = []
 titles = []
 types = []
 
-
 # creating a loop counter
 i = 0
 
-print('-------------------------- Beginning of Scraping --------------------------')
-
 # Loop through each URL in note_urls
 for url in full_urls:
-    
-    #wait 3 secondes between requests
-    time.sleep(3)
+    #wait 1 second between requests
+    time.sleep(1)
     
     # connect to every webpage
     note_resp = requests.get(url)
     
     # checking if the request is successful
-    if note_resp.status_code == 200:            # Everything is OK!
-        print('URL #{0}: {1}'.format(i+1,url))    # print out the number of iteration and the URL to keep track of place in loop
-    
-    else:                                       # Something is wrong!
+    if note_resp.status_code != 200:
         print('Status code {0}: Skipping URL #{1}: {2}'.format(note_resp.status_code, i+1, url))
         i = i+1
         continue
         
-    
     # get HTML from webpage
     note_html = note_resp.content
     
     # convert HTML to BeautifulSoup object
     note_soup = BeautifulSoup(note_html, 'lxml')
 
-    
     # find  articles on the webpage
     note_pars = note_soup.find_all('div' , class_ = 'field-item even')
     
@@ -99,40 +88,10 @@ for url in full_urls:
     
 print('-------------------------- SCRAPING DONE --------------------------')
 
-
-
 # Import pandas to create our dataframe 
 import pandas as pd
 # Create a dataframe with our scraped data
 df = pd.DataFrame(list(zip(full_urls, titles, article, types)), columns =['link', 'titles', 'article', 'type']) 
 # Create and download the csv file
-path = ('C:/Users/FAHD/Desktop/Formation/Web Scraping/AI Squad/AssabahNews/AssabahNews.csv')
+path = ('AssabahNews.csv')
 df.to_csv(path, encoding='utf-8-sig')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
