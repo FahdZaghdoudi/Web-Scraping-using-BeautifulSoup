48e1fd11e6a4f3959feffe664eb7b56510a4394d
FahdZaghdoudi <zaghdoudi.f28@gmail.com>
2020-05-05 00:20:01 +0100

    Update Assabah.py
    Improving script.

--- Assabah/Assabah.py
+++ Assabah/Assabah.py
@@ -11,9 +11,6 @@ response = requests.get(base_site)
 
 # Extracting the HTML
 html = response.content
-
-# Convert HTML to a BeautifulSoup object. This will allow us to parse out content from the HTML more easily.
-# Using the default parser as it is included in Python
 soup = BeautifulSoup(html, "html.parser")
 
 # Removing videos section from our page
@@ -30,7 +27,6 @@ aa = BeautifulSoup(str(x3), "html.parser")
 # Extract all links 
 links = aa.find_all('a')
 
-# We obtained relative URLs
 # To obtain the absolute URL address we will use urljoin
 from urllib.parse import urljoin
 relative_urls = [link.get('href') for link in links]
@@ -38,9 +34,6 @@ relative_urls = [link.get('href') for link in links]
 # Transforming to absolute path URLs
 full_urls = [urljoin(base_site, url) for url in relative_urls]
 
-#to avoid limiting the number of requests, we should tellpython to wait between each request
-#we importtime library
-import time
 # initialize lists to store titles and articles for each webpage
 article = []
 titles = []
@@ -49,34 +42,22 @@ types = []
 # creating a loop counter
 i = 0
 
-print('-------------------------- Beginning of Scraping --------------------------')
-
 # Loop through each URL in note_urls
 for url in full_urls:
-    
-    #wait 1 second between requests
-    #time.sleep(1)
-    
     # connect to every webpage
     note_resp = requests.get(url)
     
-    
     # checking if the request is successful
-    if note_resp.status_code == 200:            # Everything is OK!
-        print('URL #{0}: {1}'.format(i+1,url))    # print out the number of iteration and the URL to keep track of place in loop
-    
-    else:                                       # Something is wrong!
+    if note_resp.status_code != 200:
         print('Status code {0}: Skipping URL #{1}: {2}'.format(note_resp.status_code, i+1, url))
         i = i+1
         continue
         
-    
     # get HTML from webpage
     note_html = note_resp.content
     
     # convert HTML to BeautifulSoup object
     note_soup = BeautifulSoup(note_html, 'lxml')
-
     
     # find  articles on the webpage
     note_pars = note_soup.find_all('div' , class_ = 'field-item even')
@@ -92,44 +73,14 @@ for url in full_urls:
     article.append(str(art)[163:-15])
     titles.append(str(tit)[2:-2])
     types.append('nan')
-    
-    # Incrementing the loop counter
-    i = i+1
-    
+
+    i = i+1 
 print('-------------------------- SCRAPING DONE --------------------------')
-
-
 
 # Import pandas to create our dataframe 
 import pandas as pd
 # Create a dataframe with our scraped data
 df = pd.DataFrame(list(zip(full_urls, titles, article, types)), columns =['link', 'titles', 'article', 'type'])
 # Create and download the csv file
-path = ('C:/Users/FAHD/Desktop/Formation/Web Scraping/AI Squad/Assabah/Assabah.csv')
+path = ('Assabah.csv')
 df.to_csv(path, encoding='utf-8-sig')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
